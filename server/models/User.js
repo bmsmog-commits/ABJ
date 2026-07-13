92994e70f75b4bbf1bb9749148f0bb50b6c491c3
bms_mog <dammmy619@gmail.com>
2026-07-13 16:57:55 -0700

    Fix User model

--- server/models/User.js
+++ server/models/User.js
@@ -39,13 +39,19 @@ const userSchema = new mongoose.Schema(
 );
 
 userSchema.pre('save', async function (next) {
-  if (!this.isModified('password')) {
-    return next();
-  }
+  try {
+    if (!this.isModified('password')) {
+      return next();
+    }
 
-  const salt = await bcrypt.genSalt(10);
-  this.password = await bcrypt.hash(this.password, salt);
-  next();
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+
+    next();
+  } catch (error) {
+    console.error('BCRYPT ERROR:', error);
+    next(error);
+  }
 });
 
 userSchema.methods.matchPassword = async function (enteredPassword) {
